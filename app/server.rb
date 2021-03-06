49dc0274ce0521e10bb7a1c32b28fff4af804ad9
Leandro Maríngolo <leandronsp@gmail.com>
2021-03-06 21:54:23 +0000

    persist data in files

--- app/server.rb
+++ app/server.rb
@@ -4,6 +4,17 @@ socket = TCPServer.new(4242)
 
 puts 'Listening to the port 4242'
 
+def save_to_db(email)
+  File.write('./db/emails.txt', email)
+end
+
+def exist_in_db?(email)
+  content = File.read('./db/emails.txt')
+  emails = content.split("\n")
+
+  emails.include?(email)
+end
+
 loop do
   client = socket.accept
 
@@ -15,11 +26,20 @@ loop do
 
   if first_line == "GUARDAR email\n"
     email = second_line.chomp
+
+    save_to_db(email)
+
     response = "CRIADO\nEmail <#{email}> guardado com sucesso"
     client.puts response
   elsif first_line == "GET email\n"
     email = second_line.chomp
-    response = "OK\n#{email}"
+
+    if exist_in_db?(email)
+      response = "OK\n#{email}"
+    else
+      response = "NotFound\n"
+    end
+
     client.puts response
   end
 
