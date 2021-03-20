1c8267ad976812f91bb3997a08235bb260c66455
Leandro Proença <leandronsp@gmail.com>
2021-03-20 22:30:48 +0000

    refactoring IaC: make webserver a component

---
+++ iac-aws/webserver.js
@@ -0,0 +1,44 @@
+"use string";
+
+const aws = require("@pulumi/aws");
+const pulumi = require("@pulumi/pulumi");
+
+let config = new pulumi.Config();
+
+let securityGroup = new aws.ec2.SecurityGroup("ssh-from-allowed-ip", {
+  ingress: [
+    { protocol: "tcp", fromPort: 22, toPort: 22, cidrBlocks: [config.require("ssh-from-ip")] },
+  ],
+  egress: [
+    { fromPort: 0, toPort: 0, protocol: "-1", cidrBlocks: ["0.0.0.0/0"] },
+  ],
+  tags: {
+    Name: "ssh-from-allowed-ip",
+  }
+});
+
+let keyPair = new aws.ec2.KeyPair("playground-keypair", {
+  publicKey: config.requireSecret("ssh-public-key"),
+});
+
+let ami = pulumi.output(aws.ec2.getAmi({
+  filters: [
+    { name: "name", values: ["amzn2-ami-hvm-*"] },
+  ],
+  owners: ["137112412989"], // Amazon (public image)
+  mostRecent: true,
+}));
+
+let defaultSecurityGroup = pulumi.output(aws.ec2.getSecurityGroup({
+  name: "default",
+}));
+
+exports.createInstance = function(name, size) {
+  return new aws.ec2.Instance(name, {
+    tags: { Name: name },
+    instanceType: size,
+    vpcSecurityGroupIds: [defaultSecurityGroup.id, securityGroup.id],
+    ami: ami.id,
+    keyName: keyPair.keyName,
+  });
+}
