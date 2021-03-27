6bfdc1f0aaa0223e656623c00ca1ed4b55e1f025
Leandro Proença <leandronsp@gmail.com>
2021-03-27 21:30:27 +0000

    fix test

--- test/e2e/static_asset_test.rb
+++ test/e2e/static_asset_test.rb
@@ -2,13 +2,13 @@ class StatisAssetTest < Test::Unit::TestCase
   include ServerTestHelper
 
   def test_css_asset
-    server.puts(prepare_request("GET /public/yata.css"))
+    server.puts(prepare_request("GET /app/frontend/stylesheets/yata.css"))
 
     assert remove_cr(response).match(/HTTP\/1\.1 200.*?/)
   end
 
   def test_asset_not_found
-    server.puts(prepare_request("GET /public/wrooooong.css"))
+    server.puts(prepare_request("GET /app/frontend/stylesheets/wrooooong.css"))
 
     assert remove_cr(response).match(/HTTP\/1\.1 404.*?/)
   end
