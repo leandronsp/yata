7427301d87681c369868d0481db9ed3ec57cd849
Leandro Proença <leandronsp@gmail.com>
2021-10-23 00:40:12 +0100

    refactoring: remove iac folder

--- iac/instance-provisioning/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const pulumi = require("@pulumi/pulumi");
-const webserver = require("./webserver.js");
-
-let webInstance = webserver.createInstance("playground-webserver", "t3.micro");
-
-exports.webUrl = pulumi.interpolate `http://${webInstance.publicDns}`;
